756bf2df74328d1a76ba7fe78da03f35f98f1346
avansledright <avansledright@gmail.com>
2023-12-01 14:56:34 -0600

    adding files

---
+++ codebuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "AUTOMATED LAMBDA TESTING PIPELINE"
+
+lambda_functions=$(jq .Lambdas[] lambda_tests.json)
+length=$(jq '.Lambdas | length' lambda_tests.json)
+
+echo "TOTAL LAMBDAS = $length"
+
+
+lambda_info=$(jq .Lambdas lambda_tests.json)
+str_lambda=$(echo $lambda_info | jq -r tostring)
+python3 main.py $str_lambda
+
+echo "FINISHED TESTING"
---
+++ main.py
@@ -0,0 +1,61 @@
+import boto3
+from botocore.exceptions import ClientError
+import sys
+import base64
+import json
+import os 
+
+def send_sns(message):
+    client = boto3.client("sns", region_name="us-west-2")
+    try:
+        client.publish(
+            TopicArn=os.environp['sns_topic'],
+            Message=message,
+            Subject='Automated Lambda Testing Results'
+        )
+        return True
+    except ClientError as e:
+        print("Failed to send message to SNS")
+        print(e)
+
+
+def lambda_invoke(payload, function_name):
+    client = boto3.client('lambda', region_name="us-west-2")
+    try:
+        response = client.invoke(
+            FunctionName=function_name,
+            InvocationType='RequestResponse',
+            LogType='Tail',
+            Payload=json.dumps(payload)
+        )
+        return response
+    except ClientError as e:
+        print("Failed to invoke Lambda")
+        print(e)
+        return False
+
+if __name__ == "__main__":
+    print("Starting lambda testing")
+    lambda_info = json.loads(sys.argv[1])
+    print(lambda_info)
+    results = {}
+    for _lambda in lambda_info:
+        print(_lambda)
+        for lambda_name, test_event in _lambda.items():
+            response = lambda_invoke(test_event, lambda_name)
+            if  response != False:
+                response_payload = json.loads(response['Payload'].read())
+                results.update(
+                    {
+                        lambda_name: {
+                            'Response': response_payload,
+                            'StatusCode': response['StatusCode']
+                        }
+                    }
+                )
+    print("Sending results to SNS")
+    if send_sns(results) == True:
+        print("Successfully sent message")
+    else:
+        print("Failed to send results")  
+    print(results)  
